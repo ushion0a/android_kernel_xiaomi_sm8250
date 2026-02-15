e431d334bbb3c4ee5533d12387a69bf9ad68f585
John Doe <johndoe@example.com>
2026-02-15 18:31:48 +0800

    fix

--- build.sh
+++ build.sh
@@ -121,17 +121,17 @@ make $MAKE_ARGS ${TARGET_DEVICE}_defconfig
 if [ $KSU_ENABLE -eq 1 ]; then
     scripts/config --file out/.config \
     -e KSU \
-    -e THREAD_INFO_IN_TASK \
-    -e KSU_SUSFS \
-    -e KSU_SUSFS_SUS_PATH \
-    -e KSU_SUSFS_SUS_MOUNT \
-    -e KSU_SUSFS_SUS_KSTAT \
-    -e KSU_SUSFS_SPOOF_UNAME \
-    -e KSU_SUSFS_ENABLE_LOG \
-    -e KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS \
-    -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
-    -e KSU_SUSFS_OPEN_REDIRECT \
-    -e KSU_SUSFS_SUS_MAP 
+    -e THREAD_INFO_IN_TASK 
+#    -e KSU_SUSFS \
+#    -e KSU_SUSFS_SUS_PATH \
+#    -e KSU_SUSFS_SUS_MOUNT \
+#    -e KSU_SUSFS_SUS_KSTAT \
+#    -e KSU_SUSFS_SPOOF_UNAME \
+#    -e KSU_SUSFS_ENABLE_LOG \
+#    -e KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS \
+#    -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
+#    -e KSU_SUSFS_OPEN_REDIRECT \
+#    -e KSU_SUSFS_SUS_MAP 
    
 else
     scripts/config --file out/.config -d KSU
