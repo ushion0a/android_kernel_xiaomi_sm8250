678bd010f9cb4551fce80925053539cbd36dc20f
John Doe <johndoe@example.com>
2026-03-28 01:48:38 +0800

    fix reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -317,6 +317,9 @@ DEFINE_MUTEX(system_transition_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
